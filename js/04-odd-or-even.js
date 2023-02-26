b0519dff4ce2d9c8c63043d807cd86c93c2d3a33
Daniel Angulo <dangulo4@gmail.com>
2023-02-26 09:49:53 -0700

    completed odd or even

---
+++ js/04-odd-or-even.js
@@ -0,0 +1,8 @@
+// ITERATE FROM 0 TO 15 USING REMAINDER OPERATOR TO CHECK IF EVEN OR ODD
+for (i = 0; i <= 15; i++) {
+  if (i % 2 === 0) {
+    console.log(`${i} is even`);
+  } else {
+    console.log(`${i} is odd`);
+  }
+}
